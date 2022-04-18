e0b3e27c16ecc9dec608abceea2c73837c193408
Rasl174 <98830529+Rasl174@users.noreply.github.com>
2022-04-18 15:55:24 +0500

    Изменил

--- CS_DZ_OOP_9/Program.cs
+++ CS_DZ_OOP_9/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace CS_DZ_OOP_9
 {
@@ -10,27 +7,33 @@ namespace CS_DZ_OOP_9
     {
         static void Main(string[] args)
         {
-            List<Product> products = new List<Product>() { new Product("Макароны", 150), new Product("Газировка", 200), new Product("Масло", 300) };
             List<Clients> clients = new List<Clients>() { new Clients(1000), new Clients(300), new Clients(100) };
             Magazine magazine = new Magazine();
-            magazine.Work(products, clients);
+            magazine.Work(clients);
         }
     }
 
     class Magazine
     {
-        public void Work(List<Product> products, List<Clients> clients)
+        public void Work(List<Clients> clients)
         {
+            List<Product> products = new List<Product>() { new Product("Макароны", 150), new Product("Газировка", 200), new Product("Масло", 300) };
+
             int clientNumber = 1;
 
             while (clients.Count > 0)
             {
-
+                List<Product> clientCart = new List<Product>();
                 List<Product> buyingProducts = new List<Product>();
 
                 foreach (var client in clients)
                 {
-                    client.Buy(products, buyingProducts);
+                    foreach (var product in products)
+                    {
+                        clientCart.Add(product);
+                    }
+
+                    client.Buy(clientCart, buyingProducts);
 
                     if(buyingProducts.Count > 0)
                     {
@@ -77,28 +80,53 @@ namespace CS_DZ_OOP_9
             Money = money;
         }
 
-        public void Buy(List<Product> products, List<Product> buyingProducts)
+        public void Buy(List<Product> productCart, List<Product> buyingProducts)
         {
+            int allCostProducts = 0;
+
             Random random = new Random();
 
-            var productInCart = products.OrderBy(x => random.Next()).ToList();
-
-            while(productInCart.Count > 0)
+            for (int i = productCart.Count - 1; i >= 1; i--)
             {
-                foreach (var product in productInCart)
+                int j = random.Next(i + 1);
+
+                Product productInCart = productCart[j];
+                productCart[j] = productCart[i];
+                productCart[i] = productInCart;
+            }
+
+            foreach (var product in productCart)
+            {
+                allCostProducts += product.Cost;
+            }
+
+            if(Money >= allCostProducts)
+            {
+                foreach (var product in productCart)
                 {
-                    if(product.Cost <= Money)
+                    buyingProducts.Add(product);
+                }
+                Console.WriteLine("У меня на все хватило денег!");
+            }
+            else
+            {
+                while (productCart.Count > 0)
+                {
+                    foreach (var product in productCart)
                     {
-                        Money -= product.Cost;
-                        buyingProducts.Add(product);
-                        productInCart.Remove(product);
+                        if(product.Cost <= Money)
+                        {
+                            Money -= product.Cost;
+                            buyingProducts.Add(product);
+                            productCart.Remove(product);
+                        }
+                        else
+                        {
+                            Console.WriteLine("Мне не хватает на - " + product.Name);
+                            productCart.Remove(product);
+                        }
+                        break;
                     }
-                    else
-                    {
-                        Console.WriteLine("Я не смог купить - " + product.Name);
-                        productInCart.Remove(product);
-                    }
-                    break;
                 }
             }
         }
