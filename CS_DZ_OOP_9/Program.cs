1cf7bcf4f13c504538700f18f6f2bf490a71d9ba
Rasl174 <98830529+Rasl174@users.noreply.github.com>
2022-04-18 19:59:15 +0500

    Изменил

--- CS_DZ_OOP_9/Program.cs
+++ CS_DZ_OOP_9/Program.cs
@@ -7,15 +7,15 @@ namespace CS_DZ_OOP_9
     {
         static void Main(string[] args)
         {
-            List<Clients> clients = new List<Clients>() { new Clients(1000), new Clients(300), new Clients(100) };
-            Magazine magazine = new Magazine();
-            magazine.Work(clients);
+            List<Client> clients = new List<Client>() { new Client(1000), new Client(500), new Client(100) };
+            Store store = new Store();
+            store.Work(clients);
         }
     }
 
-    class Magazine
+    class Store
     {
-        public void Work(List<Clients> clients)
+        public void Work(List<Client> clients)
         {
             List<Product> products = new List<Product>() { new Product("Макароны", 150), new Product("Газировка", 200), new Product("Масло", 300) };
 
@@ -23,35 +23,14 @@ namespace CS_DZ_OOP_9
 
             while (clients.Count > 0)
             {
-                List<Product> clientCart = new List<Product>();
-                List<Product> buyingProducts = new List<Product>();
-
+                Console.WriteLine("Клиент номер - " + clientNumber);
                 foreach (var client in clients)
                 {
-                    foreach (var product in products)
-                    {
-                        clientCart.Add(product);
-                    }
-
-                    client.Buy(clientCart, buyingProducts);
-
-                    if(buyingProducts.Count > 0)
-                    {
-                        Console.WriteLine("Клиент - " + clientNumber + " купил");
-
-                        foreach (var product in buyingProducts)
-                        {
-                            Console.WriteLine(product.Name);
-                        }
-                    }
-                    else
-                    {
-                        Console.WriteLine("Мне не хватило денег на продукты!");
-                    }
+                    client.Buy(products);
                     clients.Remove(client);
-                    Console.ReadKey();
                     break;
                 }
+                Console.ReadKey();
                 clientNumber++;
             }
             Console.WriteLine("Вроде это был последний!");
@@ -71,64 +50,94 @@ namespace CS_DZ_OOP_9
         }
     }
 
-    class Clients
+    class Client
     {
         public int Money { get; private set; }
 
-        public Clients(int money)
+        public Client(int money)
         {
             Money = money;
         }
 
-        public void Buy(List<Product> productCart, List<Product> buyingProducts)
+        public void Buy(List<Product> products)
         {
+            List<Product> buyingProducts = new List<Product>();
+            List<Product> productsCart = new List<Product>();
             int allCostProducts = 0;
+
+            foreach (var product in products)
+            {
+                productsCart.Add(product);
+            }
 
             Random random = new Random();
 
-            for (int i = productCart.Count - 1; i >= 1; i--)
+            for (int i = productsCart.Count - 1; i >= 1; i--)
             {
-                int j = random.Next(i + 1);
+                int inex = random.Next(i + 1);
 
-                Product productInCart = productCart[j];
-                productCart[j] = productCart[i];
-                productCart[i] = productInCart;
+                Product productInCart = productsCart[inex];
+                productsCart[inex] = productsCart[i];
+                productsCart[i] = productInCart;
             }
 
-            foreach (var product in productCart)
+            foreach (var product in productsCart)
             {
                 allCostProducts += product.Cost;
             }
 
             if(Money >= allCostProducts)
             {
-                foreach (var product in productCart)
+                foreach (var product in productsCart)
                 {
                     buyingProducts.Add(product);
+                    Pay(product.Cost);
                 }
                 Console.WriteLine("У меня на все хватило денег!");
             }
             else
             {
-                while (productCart.Count > 0)
+                while (productsCart.Count > 0)
                 {
-                    foreach (var product in productCart)
+                    foreach (var product in productsCart)
                     {
                         if(product.Cost <= Money)
                         {
-                            Money -= product.Cost;
                             buyingProducts.Add(product);
-                            productCart.Remove(product);
+                            Pay(product.Cost);
+                            productsCart.Remove(product);
                         }
                         else
                         {
                             Console.WriteLine("Мне не хватает на - " + product.Name);
-                            productCart.Remove(product);
+                            productsCart.Remove(product);
                         }
                         break;
                     }
                 }
             }
+            ShowBuyingProducts(buyingProducts);
+        }
+
+        public void ShowBuyingProducts(List<Product> products)
+        {
+            if (products.Count > 0)
+            {
+                Console.WriteLine("Я купил:");
+                foreach (var product in products)
+                {
+                    Console.WriteLine(product.Name);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Мне ни на что не хватило!");
+            }
+        }
+
+        public void Pay(int cost)
+        {
+            Money -= cost;
         }
     }
 }
